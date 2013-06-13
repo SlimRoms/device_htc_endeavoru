8213cbbd8946380660d147213eef2b3f487dca39
Muhammed Nazim <mnm9994u@gmail.com>
2013-06-13 12:34:14 +0200

    Fixed OMAP4 camera HALs conflicting

--- camerawrapper/Android.mk
+++ camerawrapper/Android.mk
@@ -1,3 +1,5 @@
+ifeq ($(TARGET_DEVICE),endeavoru)
+
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
@@ -13,3 +15,5 @@ LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
 #include $(BUILD_HEAPTRACKED_SHARED_LIBRARY)
+
+endif
